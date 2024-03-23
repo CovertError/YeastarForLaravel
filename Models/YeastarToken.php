fea7ce596f4c7fe949425bb377b9af3a7af788da
coverterror <coverterror@gmail.com>
2024-03-24 00:39:42 +0300

    fixed larastan issues

--- Models/YeastarToken.php
+++ Models/YeastarToken.php
@@ -4,12 +4,19 @@ namespace Coverterror\YeastarForLaravel\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
 /**
+ * @property string $access_token
+ * @property Carbon|null $access_token_expire_time
+ * @property string $refresh_token
+ * @property Carbon|null $refresh_token_expire_time
+ * @property string $token
  * @mixin Builder
  */
 class YeastarToken extends Model
 {
-    protected $table = 'yeastar_tokens'; // Specify the table name if it's not the pluralized form of the model name
+    protected $table = 'yeastar_tokens';
 
     protected $fillable = [
         'access_token',
--- src/Facades/Yeastar.php
+++ src/Facades/Yeastar.php
@@ -2,10 +2,18 @@
 
 namespace Coverterror\YeastarForLaravel\Facades;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Coverterror\YeastarForLaravel\Yeastar
+ */
+/**
+ * @property string $access_token
+ * @property Carbon|null $access_token_expire_time
+ * @property string $refresh_token
+ * @property Carbon|null $refresh_token_expire_time
+ * @property string $token
  */
 class Yeastar extends Facade
 {
